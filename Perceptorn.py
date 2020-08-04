cdeda96b675d26bb57af1aa6e13753ce48401f6a
hjkim <hyeonjong_kim@kangwon.ac.kr>
2020-08-04 22:19:57 +0900

    PerceptornEX_3

--- Perceptorn.py
+++ Perceptorn.py
@@ -18,6 +18,16 @@ def AND_addBias(x1, x2):
     else:
         return 1
 
+def OR(x1, x2):
+    x = np([x1, x2])
+    w = np([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
 def NAND(x1, x2):
     x = np([x1, x2])
     w = np([-0.5, -0.5])
@@ -27,3 +37,9 @@ def NAND(x1, x2):
         return 0
     else:
         return 1
+
+def XOR(x1,x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
