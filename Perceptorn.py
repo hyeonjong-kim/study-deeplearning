09bb3630d8027a97304eb8b9a10ffaf1d58ea351
hjkim <hyeonjong_kim@kangwon.ac.kr>
2020-08-04 22:07:03 +0900

    PerceptornEX_2

--- Perceptorn.py
+++ Perceptorn.py
@@ -1,7 +1,29 @@
+import numpy as np
+
 def AND(x1, x2):
     w1, w2, theta = 0.5, 0.5, 0.7
     tmp = x1*w1 + x2*w2
     if tmp <= theta:
         return 0
     elif tmp > theta:
+        return 1
+
+def AND_addBias(x1, x2):
+    x = np([x1, x2])
+    w = np([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def NAND(x1, x2):
+    x = np([x1, x2])
+    w = np([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
         return 1
